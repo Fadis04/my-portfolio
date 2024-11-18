6de1665a7773f4dd0a13273ddf3a824c5ffab1d4
Fadis04 <fadi.slimi@insat.ucar.tn>
2024-11-18 14:34:56 +0100

    updated resume 2

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import cv from "../../assets/Resume_Fadi_Slimi.pdf";
+import cv from "../../assets/Resume_Fadi_SLIMI.pdf";
 const CTA = () => {
   return (
     <div className="cta">
